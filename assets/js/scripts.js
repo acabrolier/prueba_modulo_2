42db82be840d8e9cbcb625f45a4be887ddb1a5e4
Antonella Cabrolier <tocahur@gmail.com>
2024-07-17 09:18:34 -0400

    Primer versionado de prueba M2; se insertan archivos ya desarrollados

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$('#button01').on('click', function () {
+    $('#seccion01').slideToggle(1000);
+})
+$('#button02').on('click', function () {
+    $('#seccion02').slideToggle(1000);
+})
+$('#button03').on('click', function () {
+    $('#seccion03').slideToggle(1000);
+})
